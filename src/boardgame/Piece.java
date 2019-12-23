a48aaa0efcdd011b1f44efcc454b77a90d01a895
aprdev <jonasfg1319@hotmail.com>
2019-12-22 21:02:43 -0300

    Starting to implement Board and Piece

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,19 @@
+package boardgame;
+
+public class Piece {
+   protected Position position;
+   private Board board;
+   
+   public Piece(Board board) 
+   {
+	   this.board = board;
+	   position = null; //não precisa colocar, pq por padrão ja vai ser nulo
+   }
+    
+   public Board getBoard() 
+   {
+	   return board;
+   }
+   
+   //meu tabuleiro não vai ser alterado, por isso não vamos precisar do SET apenas do GET
+}
