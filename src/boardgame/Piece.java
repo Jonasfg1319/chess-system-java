d8fa86798921cd02ec799d366845525b40fc53cd
aprdev <jonasfg1319@hotmail.com>
2020-01-02 21:16:51 -0300

    Possible moves of a piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,8 +1,9 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
    protected Position position;
    private Board board;
+   
    
    public Piece(Board board) 
    {
@@ -15,5 +16,29 @@ public class Piece {
 	   return board;
    }
    
+  
+   
    //meu tabuleiro não vai ser alterado, por isso não vamos precisar do SET apenas do GET
-}
+
+   public abstract boolean[][] possibleMoves();
+   
+   public boolean possibleMove(Position position) {
+	   return possibleMoves()[position.getColumn()][position.getRow()];   
+   }
+   
+   public boolean isThereAnyPossibleMove() {
+	   boolean[][] mat = possibleMoves();
+	   for(int i=0; i < mat.length; i++) {
+		   for(int j = 0; j< mat.length; j++) {
+		          if(mat[i][j] == true) {
+		        	  return true;
+		          }
+		      }
+		   }
+		   return false;
+	   }
+   }
+
+
+
+
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -45,6 +45,8 @@ public class ChessMatch {
 	private void  validateSourcePosition(Position position) {
 		if(!board.thereIsAPiece(position)) {
 			throw new ChessException("there is no piece on source position");
+		}if(!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("ther is no possibles moves for the chosen piece");
 		}
 	}
 	
